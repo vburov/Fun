3ee7266205df5fcf80c762dacb9f744de210e194
Vasiliy Burov <vasebur@gmail.com>
2019-03-02 19:07:55 +0300

    Update eventlogcurse.ps1

--- eventlogcurse.ps1
+++ eventlogcurse.ps1
@@ -2,7 +2,7 @@ $Log_name = (Get-WinEvent -ListLog * -Force).LogName
 for ($i=0; $i -lt $Log_name.length; $i++)
 {
 $Log = Get-WinEvent -ListLog $Log_name[$i]
-if (!$Log.IsEnable)
+if (!$Log.IsEnabled)
 {
 $Log.IsEnabled = $true
 $Log.SaveChanges()
